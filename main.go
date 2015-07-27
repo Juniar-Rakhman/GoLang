69965b9e6e241326fe6a836bb93a2e2c2e0ea24c
juniar.rakhman <juniar.rahman@gmail.com>
2015-07-27 09:37:40 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+func Greet(salutation Salutation) {
+	message, alternate := CreateMessage(salutation.name, salutation.greeting)
+
+	fmt.Println(message)
+	fmt.Println(alternate)
+}
+
+func CreateMessage(name, greeting string) (message string, alternate string) {
+	message = greeting + " "+ name
+	alternate = "Hey " + name
+	return
+}
+
+
+func main() {
+	var s = Salutation{"Bob", "Hello"}
+	Greet(s)
+}
