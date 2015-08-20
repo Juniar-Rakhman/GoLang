fb3bb8cc1bca26cf3b6c884d03b86a3bb13dacd2
juniar.rakhman <juniar.rahman@gmail.com>
2015-08-21 00:07:23 +0800

    test 2

---
+++ greeting/greeting.go
@@ -0,0 +1,33 @@
+package greeting
+
+import "fmt"
+
+type Salutation struct {
+	Name     string
+	Greeting string
+}
+
+type Printer func(string) ()
+
+func Greet(salutation Salutation, do Printer, isFormal bool) {
+	message, alternate := CreateMessage(salutation.Name, salutation.Greeting, "Hi", "yo")
+	if prefix := "Mr "; isFormal {
+		do(prefix + message)
+	} else {
+		do(alternate)
+	}
+}
+
+func CreatePrintFunction(custom string) Printer {
+	return func(s string) {
+		fmt.Println(s + custom)
+	}
+}
+
+//...means we are passing unknown number of var
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+	fmt.Println(len(greeting))
+	message = greeting[2] + " "+ name
+	alternate = "Hey " + name
+	return
+}
--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
-import "fmt"
 
-type Salutation struct {
-	name     string
-	greeting string
-}
-
-type Print func(string) ()
-
-func Greet(salutation Salutation, do func(string)) {
-	message, alternate := CreateMessage(salutation.name, salutation.greeting, "Hi", "yo")
-	do(message)
-	do(alternate)
-}
-
-func Print(s string) {
-	fmt.Print(s)
-}
-
-func PrintLine(s string) {
-	fmt.Println(s)
-}
-
-func CreateMessage(name string, greeting ...string) (message string, alternate string) {
-	fmt.Println(len(greeting))
-	message = greeting[2] + " "+ name
-	alternate = "Hey " + name
-	return
-}
+import "./greeting"
 
 func main() {
-	var s = Salutation{"Bob", "Hello"}
-	Greet(s, Print)
-	Greet(s, PrintLine)
+	var s = greeting.Salutation{"Bob", "Hello"}
+	greeting.Greet(s, greeting.CreatePrintFunction(" !!!"), true)
 }
+
